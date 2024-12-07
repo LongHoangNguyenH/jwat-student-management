5a9c718e5a6fcbe300f5555c42b0883e334f06bd
longnh <longnh.uit@gmail.com>
2024-12-07 16:44:27 +0700

    feat/student: search Like

--- src/route/studentRoute.js
+++ src/route/studentRoute.js
@@ -115,4 +115,23 @@ router.get('/class/:className', (req, res) => {
   }
 });
 
+//get student by nam
+router.get('/name/:studentName', (req, res) => {
+  try {
+    const studentName = req.params.studentName;
+    if(studentName === '') {
+      return res.status(400).json({ message: 'Student name is required' });
+    }
+    searchLike = [];
+    listStudent.filter(student => {
+      if(student.studentName.toLowerCase().includes(studentName.toLowerCase())) {
+        searchLike.push(student);
+      }
+    })
+    res.status(200).json({ message: 'Students found by name', data: searchLike})
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: 'Error occurred while getting student by name' });
+  }
+});
 module.exports = router;
